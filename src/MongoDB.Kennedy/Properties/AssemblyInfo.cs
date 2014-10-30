4cb4376aa6a379c91f4ab8e0d68cf30a5b31efe3
Michael Kennedy <mikeckennedy@gmail.com>
2014-10-30 10:08:11 -0700

    Updated nuget mongodb libs.

--- src/MongoDB.Kennedy/Properties/AssemblyInfo.cs
+++ src/MongoDB.Kennedy/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.1.10.0")]
-[assembly: AssemblyFileVersion("1.1.10.0")]
+[assembly: AssemblyVersion("1.1.10.2")]
+[assembly: AssemblyFileVersion("1.1.10.2")]
